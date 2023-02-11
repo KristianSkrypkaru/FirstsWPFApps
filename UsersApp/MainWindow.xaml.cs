4637975cdcfa2a4aca79bb170c30a8bf1a73f85e
Kristian Skrypkaru <kristian.skrypkaru@outlook.com>
2023-02-11 11:53:28 +0200

    added DB(SQLite) Users

--- UsersApp/MainWindow.xaml.cs
+++ UsersApp/MainWindow.xaml.cs
@@ -21,5 +21,47 @@ namespace UsersApp
         {
             InitializeComponent();
         }
+
+        private void Button_Sign_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string password = passwordBox.Password.Trim();
+            string password_2 = passwordBox.Password.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Login is not correct";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (password.Length < 5) 
+            {
+                passwordBox.ToolTip = "The password is not correct";
+                passwordBox.Background = Brushes.DarkRed;
+            }
+            else if (password != password_2)
+            {
+                passwordBox_2.ToolTip = "Password doesn't match";
+                passwordBox_2.Background = Brushes.DarkRed;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Email is not correct";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passwordBox.ToolTip = "";
+                passwordBox.Background = Brushes.Transparent;
+                passwordBox_2.ToolTip = "";
+                passwordBox_2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Everything's okay");
+            }
+        }
     }
 }
