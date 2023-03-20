4d3844c889326a35853987461b2c130f5e498a52
Kristian Skrypkaru <kristian.skrypkaru@outlook.com>
2023-03-20 22:21:43 +0200

    added new project MobileApp

---
+++ MobileApp/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MobileApp
+{
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region On Loaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Get every logical drive on the machine
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                // Create a new item for it
+                var item = new TreeViewItem();
+
+                // Set the header
+                item.Header = drive;
+                // And the full path
+                item.Tag = drive;
+
+                // Add dummy item
+                item.Items.Add(null);
+
+                // Listen out for item being expanded
+                item.Expanded += Folder_Expanded;
+
+                // Add it to the main tree-view
+                FolderView.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+           var item = (TreeViewItem)sender;
+
+            // If the item only contains the dummy data
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+            
+            // Clear dummy data
+            item.Items.Clear();
+
+            // Get full path
+            var fullPath = (string)item.Tag;
+
+            var directories = new List<string>();
+
+            try
+            {
+                foreach (var directoryPath in Directory.GetDirectories(fullPath))
+                {
+
+                }
+            }
+            catch{ }
+        }
+
+        #endregion
+    }
+}
